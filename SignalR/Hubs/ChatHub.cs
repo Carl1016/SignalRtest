aee20c3563127a65c13a54bce0219baf3cce4910
CarlChiang <410335011@gms.ndhu.edu.tw>
2019-07-24 16:26:44 +0800

    make it better

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            var content = $"{user} 於{DateTime.Now.ToShortTimeString()}說：{message}";
+
+            await Clients.All.SendAsync("ReceiveMessage", content);
+        }
+    }
+}
